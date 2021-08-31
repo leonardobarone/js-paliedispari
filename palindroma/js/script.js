1d25d295108b522b7328ea1996549dacb15114ad
leonardobarone <leonardobarone26@gmail.com>
2021-08-31 16:48:56 +0200

    esercizio palindromi fatto male

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,3 +1,28 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// PALINDROMA
+
+// CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
+// var parola = prompt("inserisci una parola").toLowerCase();
+// while (!isNaN(parola)) {
+//     var parola = prompt("inserisci una parola").toLowerCase();
+// }
+
+var parola = "ciao";
+// CREA UNA FUNZIONE PER CAPIRE SE LA PAROLA INSERITA È PALINDROMA
+function isPalindromea(string) {
+    
+    var control = false;
+
+    for (let i = 0; i < string.length / 2; i++) {
+
+        if (string[i] == string[string.length - 1 - i]) {
+            control = true;
+            alert( 'è palindroma'); break
+        }
+    }
+        if (control == false) {
+            alert( 'non è palindroma');
+        }
+
+}
+
+console.log(isPalindromea("ciao"));
