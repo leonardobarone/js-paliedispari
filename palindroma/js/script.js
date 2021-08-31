0beeb8e634b0a1aef6d3d67b2897882b75cbedf5
leonardobarone <leonardobarone26@gmail.com>
2021-08-31 16:55:32 +0200

    corretto, ma riesco a farlo funzionare solo con break

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,28 +1,27 @@
 // PALINDROMA
 
 // CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
-// var parola = prompt("inserisci una parola").toLowerCase();
-// while (!isNaN(parola)) {
-//     var parola = prompt("inserisci una parola").toLowerCase();
-// }
+var parola = prompt("inserisci una parola").toLowerCase();
+while (!isNaN(parola)) {
+    var parola = prompt("inserisci una parola").toLowerCase();
+}
 
-var parola = "ciao";
 // CREA UNA FUNZIONE PER CAPIRE SE LA PAROLA INSERITA È PALINDROMA
-function isPalindromea(string) {
+function palindroma(string) {
     
     var control = false;
 
-    for (let i = 0; i < string.length / 2; i++) {
+    for (var i = 0; i < string.length / 2; i++) {
 
         if (string[i] == string[string.length - 1 - i]) {
             control = true;
-            alert( 'è palindroma'); break
+            return  'è palindroma'; break
         }
     }
         if (control == false) {
-            alert( 'non è palindroma');
+            return 'non è palondroma'
         }
 
 }
 
-console.log(isPalindromea("ciao"));
+console.log(palindroma(parola));
